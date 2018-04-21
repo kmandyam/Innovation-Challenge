88b36e13c39a0e76f7755c25fae06d80d790aaf1
Karishma Mandyam <karishma.mandyam@gmail.com>
2018-04-21 16:11:47 -0700

    I think everything works

--- samples/browser/sample-server.js
+++ samples/browser/sample-server.js
@@ -8,7 +8,7 @@ var qrcode = require('qrcode-terminal'); // "qrcode-terminal": "^0.11.0",
 var sample = fs.readFileSync(__dirname + '/Sample.html', 'utf8');
 
 var enableTunnel = false;
-for (let j = 0; j < process.argv.length; j++) {  
+for (let j = 0; j < process.argv.length; j++) {
     enableTunnel |= process.argv[j] == 'enableTunnel';
 }
 
@@ -17,7 +17,7 @@ if (fs.existsSync(keyFile)) {
     var key = fs.readFileSync(keyFile, 'utf8');
     key = key.replace(/\s/g, "");
     if (!!key) {
-        var before = "value=\"YOUR_BING_SPEECH_API_KEY\"";
+        var before = "value=\"81dcb46dec864016b26c277e88b0c266\"";
         var after = " disabled value=\"Using token-based auth mechanism.\"";
         sample = sample.replace(before, after);
         sample = sample.replace('var useTokenAuth = false;', 'var useTokenAuth = true;');
@@ -36,11 +36,11 @@ var server = http.createServer(function(request, response){
         response.end();
     }
 
-    path= url.parse(request.url).pathname;    
+    path= url.parse(request.url).pathname;
     console.log("Incoming request:" + request.url);
 
     if (path == '/token') {
-        getToken(key, function(token){ 
+        getToken(key, function(token){
             respond(200, token);
          })
     } else if (path == '/') {
@@ -48,7 +48,7 @@ var server = http.createServer(function(request, response){
     } else {
         var pathExists = fs.existsSync(__dirname + '/../../'+path);
 
-        if (!pathExists || 
+        if (!pathExists ||
             !path.endsWith('speech.sdk.bundle.js') &&
             !path.endsWith('speech.sdk.bundle.js.map')) {
             respond(404);
@@ -66,7 +66,7 @@ if (enableTunnel) {
     var tunnel = localtunnel(port, function(err, tunnel) {
         if (err) {
             quit = true;
-            server.close(); 
+            server.close();
             console.log('Something went south...' + err.message)
         } else {
             printServerInfo(tunnel.url)
@@ -75,7 +75,7 @@ if (enableTunnel) {
 
     tunnel.on('close', function() {
         quit = true;
-        server.close();   
+        server.close();
     });
 } else {
     printServerInfo('http://'+os.hostname() + ':' + port);
